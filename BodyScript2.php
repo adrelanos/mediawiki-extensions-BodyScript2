5e0c8ac18d8cd3e38ec932edd2de5454d760be24
Patrick Schleizer <adrelanos@whonix.org>
2022-01-10 10:22:59 -0500

    fix

--- BodyScript2.php
+++ BodyScript2.php
@@ -1,16 +1,7 @@
-<?php
-
-class BodyScript2 {
-
-	/**
-	 * Code for adding the head script to the wiki
-	 *
-	 * @param OutputPage &$out
-	 */
-   #public static function onOutputPageParserOutput( OutputPage &$out, ParserOutput $parserOutput ) {
-   public static function onAfterFinalPageOutput( &$output )
-		global $wgBodyScript2Code;
-
-		$output->addHTML( $wgBodyScript2Code );
-	}
+public static function onAfterFinalPageOutput( $out ) {
+   $out = ob_get_clean();
+   ob_start();
+   $out .= $wgBodyScript2Code;
+   echo $out;
+   return true;
 }
